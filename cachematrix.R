54c6ad31b866f3a91986fd48926718429c80738a
Rick Rick <rickajedrez@gmail.com>
2016-01-24 23:23:04 +0100

    terminado

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This R file has the solutions to Assignment 2 of
+## the R Programming Course
 
-## Write a short comment describing this function
+## This function creates a matrix and holds an internal
+## variable. It is filled when an inverse matrix is computed
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+
+## This function computes the inverse matrix when it is not filled
+## otherwise ir gets a previously computed value, It advices 
+## when the cached value is used
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached inverse matrix")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
+  
 }
